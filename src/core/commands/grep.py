6d75bf75e1a5f66325548b18fac5b9772d3231db
InchanterX <inchanterx.work@gmail.com>
2025-10-25 12:34:11 +0300

    Finally fixed regular expressions.

---
+++ src/core/commands/grep.py
@@ -0,0 +1,21 @@
+# import src.utils.constants
+
+
+class grep:
+    '''
+    Command "grep"
+    '''
+
+    def __init__(self) -> None:
+        pass
+
+    def grep(self) -> None:
+        pass
+
+
+COMMAND_INFO = {
+    "name": "grep",
+    "flags": ["l", "c", "help"],
+    "description": "Search.",
+    "function": grep
+}
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,6 @@
 import os
 import re
+import pprint
 from src.utils.registry import Registry
 
 USER_HOME_DIR = os.path.expanduser("~")
@@ -8,6 +9,8 @@ USER_HOME_DIR = os.path.expanduser("~")
 REGISTRY = Registry().registration()
 NAMES = list(REGISTRY.keys())
 FLAGS = set(flag for name in NAMES for flag in REGISTRY[name]["flags"])
+
+pprint.pprint(REGISTRY)
 
 # Regular expression for spaces
 PURE_SPACE_RE = r"(\s+)"
@@ -20,19 +23,28 @@ COMMANDS_RE = f"(?P<COMMAND>{PURE_COMMANDS_RE})"
 # Regular expression for flags
 SHORT_FLAGS = [flag for flag in FLAGS if len(flag) == 1]
 LONG_FLAGS = [flag for flag in FLAGS if len(flag) > 1]
-PURE_SHORT_FLAGS_RE = f"-[{''.join(re.escape(short_flag) for short_flag in SHORT_FLAGS)}]+"
-PURE_LONG_FLAGS_RE = f"--({'|'.join(re.escape(long_flag) for long_flag in LONG_FLAGS)})"
+# PURE_SHORT_FLAGS_RE = f"-[{''.join(re.escape(short_flag) for short_flag in SHORT_FLAGS)}]+"
+PURE_SHORT_FLAGS_RE = f"-[a-zA-Z]+"
+# PURE_LONG_FLAGS_RE = f"--({'|'.join(re.escape(long_flag) for long_flag in LONG_FLAGS)})"
+PURE_LONG_FLAGS_RE = f"--[a-zA-Z]+"
 SHORT_FLAGS_RE = f"(?P<SHORT_FLAG>{PURE_SHORT_FLAGS_RE})"
 LONG_FLAGS_RE = f"(?P<LONG_FLAG>{PURE_LONG_FLAGS_RE})"
 
 # Regular expression for parameters
-PURE_UNQUOTED_PARAMETERS_RE = r"[a-zA-Zа-яА-ЯёЁ0-9_\-./\\~\(\)]+"
-PURE_QUOTED_PARAMETERS_RE = r"'[a-zA-Zа-яА-ЯёЁ0-9_\-./\\~\(\)]+'|\"[a-zA-Zа-яА-ЯёЁ0-9_\-./\\~\(\)]+\""
+#!!! Проверить, все ли символы разрешённые в путях вписаны в регулярку
+PURE_UNQUOTED_PARAMETERS_RE = r"[a-zA-Zа-яА-ЯёЁ0-9_\-./\\~\(\):]+"
+# PURE_QUOTED_PARAMETERS_RE = r"'[a-zA-Zа-яА-ЯёЁ0-9_\-./\\~\(\)]+'|\"[a-zA-Zа-яА-ЯёЁ0-9_\-./\\~\(\)]+\""
+PURE_QUOTED_PARAMETERS_RE = r"'[^']*'|\"[^\"]*\""
 UNQUOTED_PARAMETERS_RE = f"(?P<UNQUOTED_PARAMETER>{PURE_UNQUOTED_PARAMETERS_RE})"
 QUOTED_PARAMETERS_RE = f"(?P<QUOTED_PARAMETER>{PURE_QUOTED_PARAMETERS_RE})"
 
 # Regular expression for all that remained
 UNKNOWN_RE = f"(?P<UNKNOWN>.)"
+# UNKNOWN1_RE = f"(?P<UNKNOWN1>.)"
+# UNKNOWN2_RE = f"(?P<UNKNOWN2>.)"
+# UNKNOWN3_RE = f"(?P<UNKNOWN3>.)"
+# UNKNOWN4_RE = f"(?P<UNKNOWN4>.)"
+
 
 # Ordered regular expressions
 ALL_EXPRESSIONS = [COMMANDS_RE, LONG_FLAGS_RE, SHORT_FLAGS_RE,
@@ -40,6 +52,13 @@ ALL_EXPRESSIONS = [COMMANDS_RE, LONG_FLAGS_RE, SHORT_FLAGS_RE,
 
 # Master regular expression to gather all in one
 MASTER_RE = re.compile('|'.join(ALL_EXPRESSIONS))
+# MASTER_RE = re.compile((COMMANDS_RE | UNKNOWN_RE)((SPACE_RE)+|UNKNOWN_RE)((LONG_FLAGS_RE | SHORT_FLAGS_RE | QUOTED_PARAMETERS_RE | UNQUOTED_PARAMETERS_RE | UNKNOWN_RE)(SPACE_RE | UNKNOWN_RE)+)+)
+# MASTER_RE = re.compile(
+#     rf"^(?:{COMMANDS_RE}|{UNKNOWN1_RE})"
+#     rf"(?:{SPACE_RE}+|{UNKNOWN2_RE})"
+#     rf"(?:(?:{LONG_FLAGS_RE}|{SHORT_FLAGS_RE}|{QUOTED_PARAMETERS_RE}|{UNQUOTED_PARAMETERS_RE}|{UNKNOWN3_RE})"
+#     rf"(?:{SPACE_RE}+|{UNKNOWN4_RE}))+$"
+# )
 
 
 def tokenize_with_your_code(command_line):
@@ -55,8 +74,14 @@ def tokenize_with_your_code(command_line):
 
 # Тестовые строки
 test_commands = [
-    "ls 'D:\\My folder\\файлы' -la --help",
-    "ls '/dir/'another one'/gol' -la --help"
+    "ls 'D:\My folder\файлы' -la --help",
+    "ls '/dir/another one/gol' -la --help",
+    "ls D:\My_folder\файлы -la --help",
+    "ls D:\My folder\файлы -la --help",
+    "cd D:\My folder\файлы -la --help",
+    "example D:\My_folder\файлы -lak --help",
+    "grep '^\"d2' file_name.py",
+    "ls 'abc fd -ls"
 ]
 
 print("Токенизация с помощью вашего кода:\n")
