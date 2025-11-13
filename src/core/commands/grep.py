0776e6c7a67208a9d68712a7ca78e2f711fb8946
InchanterX <inchanterx.work@gmail.com>
2025-11-13 18:11:34 +0300

    Finished grep command

--- src/core/commands/grep.py
+++ src/core/commands/grep.py
@@ -1,5 +1,6 @@
 from src.services.help_call import Helper
 from src.services.path_normalizer import Normalizer
+import src.infrastructure.constants as constants
 import logging
 import re
 import os
@@ -18,6 +19,68 @@ class grep:
         self._helper = helper
         self._logger = logging.getLogger(__name__)
 
+    def _process_regular_expression(self, regular_expression: str, ignore_case: bool) -> re.Pattern[str]:
+        '''
+        Get regular expression and use compile on ot for more convenient usage in future.
+        If given expression is invalid function will return a error.
+        '''
+        try:
+            if ignore_case:
+                prepared_regular_expression = re.compile(
+                    regular_expression, flags=re.IGNORECASE)
+            else:
+                prepared_regular_expression = re.compile(
+                    regular_expression)
+        except re.error:
+            self._logger.exception(
+                f"{regular_expression} is invalid and can't be processed.")
+            raise SyntaxError(
+                f"Pattern {regular_expression} can't be processed. It is invalid.")
+        return prepared_regular_expression
+
+    def _process_file(self, path: str, original_path: str, regular_expression: re.Pattern[str]) -> list[str]:
+        '''
+        Find all the matches in lines of given file
+        and return them in numerated list of strings.
+        '''
+        try:
+            output = []
+            with open(path, "r", encoding="UTF-8", errors="ignore") as file:
+                for order, line in enumerate(file):
+                    for match in regular_expression.finditer(line):
+                        output.append(
+                            f"{file.name} {order+1} {match.group(0).rstrip()}")
+        except PermissionError:
+            self._logger.error(
+                f"Unable to open file {path}. Permission denied.")
+            output.append(
+                f"grep: cannot search in {original_path}. Permission denied.")
+        return output
+
+    def _process_directory(self, path: str, original_path: str, regular_expression: re.Pattern[str]) -> list[str]:
+        files = os.listdir(path)
+        output = []
+        for file in files:
+            try:
+                nested_path = os.path.join(path, file)
+                nested_original_path = os.path.join(original_path, file)
+                if not os.path.exists(nested_path):
+                    self._logger.error(f"File {nested_path} doesn't exists.")
+                    output.append(
+                        f"grep: File {nested_original_path} can't be processed. It doesn't exist.")
+                elif os.path.isfile(nested_path):
+                    output += (self._process_file(nested_path,
+                                                  nested_original_path, regular_expression))
+                else:
+                    output += (self._process_directory(nested_path,
+                                                       nested_original_path, regular_expression))
+            except PermissionError:
+                self._logger.exception(
+                    f"There is no permissions to {path}.")
+                output.append(
+                    f"grep: cannot search in {original_path}. Permission denied.")
+        return output
+
     def grep(self, long_flags: list[str], parameters: list[str]) -> str:
         self._logger.debug(
             f"Running grep with flags={long_flags}, parameters={parameters}")
@@ -32,8 +95,7 @@ class grep:
             raise SyntaxError("grep: No parameters were given.")
 
         if len(parameters) == 1:
-            self._logger.error("Not enough parameters was given.")
-            raise SyntaxError("grep: Not enough parameters was given.")
+            parameters.append(constants.CURRENT_DIR)
 
         # processing flags
         recursive = False
@@ -45,9 +107,8 @@ class grep:
             ignore_case = True
 
         # processing given regular expression
-        regular_expression = parameters[0]
-        regular_expression = _process_regular_expression(
-            regular_expression, ignore_case)
+        regular_expression: re.Pattern[str] = self._process_regular_expression(
+            parameters[0], ignore_case)
 
         # processing every given path
         output = []
@@ -62,50 +123,23 @@ class grep:
                         f"grep: File {original_path} can't be processed. It doesn't exist.")
                 # process files with given regular expression
                 elif os.path.isfile(path):
-                    output += _process_file(path,
-                                            original_path, regular_expression)
+                    output += (self._process_file(path,
+                                                  original_path, regular_expression))
+                elif recursive:
+                    output += (self._process_directory(path,
+                                                       original_path, regular_expression))
+                else:
+                    self._logger.error(
+                        f"Folder {path} can't be processed without recursive flag.")
+                    output.append(
+                        f"grep: folder {original_path} can't be processed without --recursive flag.")
             # if program failed to process file it is unaccessible
             except PermissionError:
                 self._logger.exception(
                     f"There is no permissions to {path}.")
                 output.append(
                     f"grep: cannot search in {original_path}. Permission denied.")
-
-        def _process_regular_expression(regular_expression: str, ignore_case: bool) -> str:
-            '''
-            Get regular expression and use compile on ot for more convenient usage in future.
-            If given expression is invalid function will return a error.
-            '''
-            try:
-                if ignore_case == True:
-                    prepared_regular_expression = re.compile(
-                        regular_expression, flags=re.IGNORECASE)
-                else:
-                    prepared_regular_expression = re.compile(
-                        regular_expression)
-            except re.error:
-                self._logger.exception(
-                    f"{regular_expression} is invalid and can't be processed.")
-                raise SyntaxError(
-                    f"Pattern {regular_expression} can't be processed. It is invalid.")
-            return prepared_regular_expression
-
-        def _process_file(path: str, original_path: str, regular_expression: str) -> list[str]:
-            '''
-            Find all the matches in lines of given file
-            and return them in numerated list of strings.
-            '''
-            try:
-                output = []
-                with open(path, "r", encoding="UTF-8", errors="ignore") as file:
-                    for order, line in enumerate(file):
-                        for match in regular_expression.finditer(line):
-                            output.append(f"{file.name} {order} {match}")
-            except PermissionError:
-                self._logger.error(
-                    f"Unable to open file {path}. Permission denied.")
-                output.append(
-                    f"grep: cannot search in {original_path}. Permission denied.")
+        return "\n".join(output)
 
 
 COMMAND_INFO = {
